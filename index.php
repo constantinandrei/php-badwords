7c3728f8a91ae4fb73a8cf6a7de1bd25f10e5c4b
constantinandrei <andrei.todirascu@gmail.com>
2022-07-07 16:16:58 +0200

    html fixed

--- index.php
+++ index.php
@@ -1,40 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Bad Words</title>
-</head>
-<body>
-    <?php
-    $myParagraph = 'He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream 
-    fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it. 
-    Nobody seemed to fully understand the beauty of this accomplishment except for the new worker who watched in amazement.';
+<?php
+$myParagraph = 'He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream 
+fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it. 
+Nobody seemed to fully understand the beauty of this accomplishment except for the new worker who watched in amazement.';
 
-    $userWord = '';
+$userWord = '';
 
-    $paragrafoCensurato = '';
-    
-    if (isset($_GET["user-word"])){
-        $userWord = $_GET["user-word"];
-    };
-    ?>
+$paragrafoCensurato = '';
 
-    <p>
-        <?php echo $myParagraph ?>
-    </p>
-    <p>
-        Lunghezza del paragrafo: <?php echo strlen($myParagraph) ?> caratteri (spazi inclusi)
-    </p>
+if (isset($_GET["user-word"])){
+    $userWord = $_GET["user-word"];
+};
+?>
 
-    <?php
-        if ($userWord !== ''){
-            $paragrafoCensurato = str_ireplace($userWord, '***', $myParagraph);
-            echo '<h5>Paragrafo censurato: </h5>';
-            echo '<p>' . $paragrafoCensurato . '</p>';
-            echo '<p>' . 'Lunghezza del paragrafo: ' . strlen($paragrafoCensurato) . ' caratteri (spazi inclusi)</p>';
-        }
-    ?>
-</body>
-</html>
+<p>
+    <?php echo $myParagraph ?>
+</p>
+<p>
+    Lunghezza del paragrafo: <?php echo strlen($myParagraph) ?> caratteri (spazi inclusi)
+</p>
+
+<?php
+    if ($userWord !== ''){
+        $paragrafoCensurato = str_ireplace($userWord, '***', $myParagraph);
+        echo '<h5>Paragrafo censurato: </h5>';
+        echo '<p>' . $paragrafoCensurato . '</p>';
+        echo '<p>' . 'Lunghezza del paragrafo: ' . strlen($paragrafoCensurato) . ' caratteri (spazi inclusi)</p>';
+    }
+?>
